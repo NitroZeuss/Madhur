476465b3fbdc307344afb54b01bb400dd880cf49
NitroZeuss <142437589+NitroZeuss@users.noreply.github.com>
2025-05-09 16:17:14 +0530

    Update settings.py

--- madhur/settings.py
+++ madhur/settings.py
@@ -63,6 +63,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'madhur.urls'
